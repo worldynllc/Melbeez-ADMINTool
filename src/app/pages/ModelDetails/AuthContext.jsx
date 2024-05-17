d5498d8866c96323679699f57e3a84c5d413e8b5
haja mydeen <120375547+haja2611@users.noreply.github.com>
2024-05-17 17:49:27 +0530

    .env, AuthContext.jsx and ProductFeed.jsx

--- src/app/pages/ModelDetails/AuthContext.jsx
+++ src/app/pages/ModelDetails/AuthContext.jsx
@@ -1,7 +1,11 @@
 // AuthProvider.js
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import { authUserDetail } from '../../services/ProfileService';
-import { showSuccessToast, showErrorToast, showWarnToast } from '../../../Utility/toastMsg'; 
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { authUserDetail } from "../../services/ProfileService";
+import {
+  showSuccessToast,
+  showErrorToast,
+  showWarnToast,
+} from "../../../Utility/toastMsg";
 
 const AuthContext = createContext();
 
@@ -18,7 +22,7 @@ export const AuthProvider = ({ children }) => {
       try {
         const response = await authUserDetail();
         if (!response.ok) {
-          throw new Error('Failed to fetch user details');
+          throw new Error("Failed to fetch user details");
         }
         const data = await response.json();
         setUserDetails(data);
@@ -34,9 +38,11 @@ export const AuthProvider = ({ children }) => {
 
   const fetchFeeds = async () => {
     try {
-      const response = await fetch("http://192.168.1.9:8083/feeds");
+      const response = await fetch(
+        `${process.env.REACT_APP_JAVA_API_URL}/feeds`
+      );
       if (!response.ok) {
-        throw new Error('Failed to fetch feeds');
+        throw new Error("Failed to fetch feeds");
       }
       const data = await response.json();
       setPostData(data);
@@ -47,48 +53,51 @@ export const AuthProvider = ({ children }) => {
 
   const handleUpload = async (formData, setMessage, setFormData, setShow) => {
     if (!formData.author) {
-      setMessage('Author is required.');
-      showWarnToast('Please fill in all required fields.');
+      setMessage("Author is required.");
+      showWarnToast("Please fill in all required fields.");
       return;
     }
-  
+
     if (!formData.file && !formData.description) {
-      setMessage('Please provide either an image or a description.');
-      showWarnToast('Please provide either an image or a description.');
-      setShow(true)
+      setMessage("Please provide either an image or a description.");
+      showWarnToast("Please provide either an image or a description.");
+      setShow(true);
       return;
     }
-  
+
     const form = new FormData();
-    form.append('author', formData.author);
+    form.append("author", formData.author);
     if (formData.file) {
-      form.append('file', formData.file);
+      form.append("file", formData.file);
     }
     if (formData.description) {
-      form.append('description', formData.description);
+      form.append("description", formData.description);
     }
     if (userDetails) {
-      form.append('userId', userDetails.result.id);
+      form.append("userId", userDetails.result.id);
     }
-  
+
     try {
-      const response = await fetch('http://192.168.1.9:8083/upload', {
-        method: 'POST',
-        body: form,
-      });
-  
+      const response = await fetch(
+        `${process.env.REACT_APP_JAVA_API_URL}/upload`,
+        {
+          method: "POST",
+          body: form,
+        }
+      );
+
       if (!response.ok) {
         const errorData = await response.json();
-        const errorMessage = errorData.error || 'Unknown error occurred.';
+        const errorMessage = errorData.error || "Unknown error occurred.";
         setMessage(errorMessage);
         showErrorToast(errorMessage);
         return;
       }
-  
-      showSuccessToast('Upload successful');
+
+      showSuccessToast("Upload successful");
       setFormData({
-        author: '',
-        description: '',
+        author: "",
+        description: "",
         file: null,
       });
       // Don't close the modal if only a description is provided
@@ -96,7 +105,7 @@ export const AuthProvider = ({ children }) => {
         setShow(false);
       }
     } catch (error) {
-      const errorMessage = 'Error uploading file: ' + error.message;
+      const errorMessage = "Error uploading file: " + error.message;
       showErrorToast(errorMessage);
       setMessage(errorMessage);
       console.error(errorMessage);
@@ -105,23 +114,38 @@ export const AuthProvider = ({ children }) => {
   };
   const handleDeletePost = async (postId) => {
     try {
-      const response = await fetch(`http://192.168.1.9:8083/feeds/id/${postId}`, {
-        method: "DELETE",
-      });
+      const response = await fetch(
+        `${process.env.REACT_APP_JAVA_API_URL}/feeds/id/${postId}`,
+        {
+          method: "DELETE",
+        }
+      );
 
       if (!response.ok) {
-        throw new Error('Failed to delete post');
+        throw new Error("Failed to delete post");
       }
 
-      setPostData((prevPosts) => prevPosts.filter((post) => post.id !== postId));
-      showSuccessToast('Post deleted successfully');
+      setPostData((prevPosts) =>
+        prevPosts.filter((post) => post.id !== postId)
+      );
+      showSuccessToast("Post deleted successfully");
     } catch (error) {
       showErrorToast("Error deleting post: " + error.message);
     }
   };
 
   return (
-    <AuthContext.Provider value={{ userDetails, loading, error, handleUpload, postData, fetchFeeds, handleDeletePost }}>
+    <AuthContext.Provider
+      value={{
+        userDetails,
+        loading,
+        error,
+        handleUpload,
+        postData,
+        fetchFeeds,
+        handleDeletePost,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
